b07e35144e9d336a66a3c161d6d8e79b606c3cc0
diegohu16 <diegohu16@gmail.com>
2024-07-08 20:09:05 +0200

    Store the entries in DB and retrieve them

--- crawler.py
+++ crawler.py
@@ -1,5 +1,39 @@
+import datetime
+import re
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
+
+
+def count_words(title):
+    # Count the number of words of the title
+    pattern = r"[^\w ]"
+
+    replaced_title = re.sub(pattern, "", title)
+
+    return len(replaced_title.split())
+
+
+def create_db():
+    # Create table to store the data
+    conn = sqlite3.connect("crawler_data.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+            CREATE TABLE IF NOT EXISTS entries (
+            rank TEXT PRIMARY KEY,
+            title TEXT,
+            points INTEGER,
+            comments INTEGER,
+            filter_type TEXT,
+            timestamp TEXT
+            )
+        """
+    )
+
+    conn.commit()
+    conn.close()
 
 
 def extract_entry_data(data, title):
@@ -7,6 +41,10 @@ def extract_entry_data(data, title):
     entry = {}
 
     entry["comments"] = data.find_all("a")[3].text.split()[0]
+
+    if entry["comments"] == "discuss":
+        entry["comments"] = 0
+
     entry["points"] = data.find("span", class_="score").text.split()[0]
     entry["rank"] = title.find("span", class_="rank").text.strip()
     entry["title"] = title.find("span", class_="titleline").find("a").text.strip()
@@ -14,7 +52,7 @@ def extract_entry_data(data, title):
     return entry
 
 
-def get_data(url):
+def extract_url_data(url):
     """Gets the html content of the given url"""
     response = requests.get(url)
     html_content = BeautifulSoup(response.content, "html.parser")
@@ -24,6 +62,68 @@ def get_data(url):
 
     for i in range(0, 29):
         entry_data = extract_entry_data(entries_data[i], entries_title[i])
+        if count_words(entry_data["title"]) > 5:
+            store_entry(entry_data, "long_titles")
+        else:
+            store_entry(entry_data, "short_titles")
 
 
-get_data("https://news.ycombinator.com/")
+def store_entry(entry, filter_type):
+    # Stores entry data along with filter type and timestamp
+    timestamp = datetime.datetime.now().isoformat()
+
+    conn = sqlite3.connect("crawler_data.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """INSERT INTO entries (rank, title, points, comments, filter_type, timestamp)
+           VALUES (?, ?, ?, ?, ?, ?)""",
+        (
+            entry["rank"],
+            entry["title"],
+            entry["points"],
+            entry["comments"],
+            filter_type,
+            timestamp,
+        ),
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_entries():
+    # Retrieves entries based on the applied filter
+    create_db()
+
+    extract_url_data("https://news.ycombinator.com/")
+
+    conn = sqlite3.connect("crawler_data.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """SELECT * FROM entries WHERE filter_type = 'long_titles' ORDER BY comments DESC"""
+    )
+    long_title_entries = cursor.fetchall()
+
+    cursor.execute(
+        """SELECT * FROM entries WHERE filter_type = 'short_titles' ORDER BY points DESC"""
+    )
+    short_title_entries = cursor.fetchall()
+
+    conn.close()
+
+    print("LONG TITLE ENTRIES:")
+    for entry in long_title_entries:
+        print(
+            f"{entry[0]} Rank. Title: {entry[1]} ({entry[2]} points, {entry[3]} comments)"
+        )
+
+    print("SHORT TITLE ENTRIES:")
+    for entry in short_title_entries:
+        print(
+            f"{entry[0]} Rank. Title: {entry[1]} ({entry[2]} points, {entry[3]} comments)"
+        )
+
+
+get_entries()
